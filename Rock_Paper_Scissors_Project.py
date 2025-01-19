155da331833bc47d623a1058b73c377eafe7005e
Mehmed Hadzhiyski <mehmedh_23@abv.bg>
2025-01-19 16:33:17 +0200

    Added option for the user to continue playing until he/she quits Added score system

--- Rock_Paper_Scissors_Project.py
+++ Rock_Paper_Scissors_Project.py
@@ -1,35 +1,50 @@
 from random import randint
+from colorama import init, Fore, Style
 
-rock, paper, scissors = "Rock", "Paper", "Scissors"
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+init()
 
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
+request = input("Would you like to start the game of Rock-Paper-Scissors? (Yes or No): ")
+player_score, computer_score = 0, 0
+
+while request == "Yes":
+    """This is the loop where the player will play against the computer until he/she quits the game"""
+    player_move = input("Choose Rock, Paper or Scissors: ")
+
+    if not player_move in ("Rock", "Paper", "Scissors"):
+        print(Fore.RED + "Invalid Input. Please try again..." + Style.RESET_ALL)
+        request = input("Would you like to continue playing? (Yes or No): ")
+        continue
+
+    computer_random_number = randint(1, 3)
+    computer_move = ""
+
+    if computer_random_number == 1:
+        computer_move = "Rock"
+    elif computer_random_number == 2:
+        computer_move = "Paper"
+    else:
+        computer_move = "Scissors"
+
+    print(Fore.BLUE + f"The computer chose {computer_move}." + Style.RESET_ALL)
+
+    if (player_move == "Rock" and computer_move == "Scissors") \
+            or (player_move == "Paper" and computer_move == "Rock") \
+            or (player_move == "Scissors" and computer_move == "Paper"):
+        player_score += 1
+        print(Fore.GREEN + "You win!" + Style.RESET_ALL)
+        print(Fore.CYAN + f"Current scores: {player_score}:{computer_score}" + Style.RESET_ALL)
+    elif player_move == computer_move:
+        print(Fore.YELLOW + "Draw!" + Style.RESET_ALL)
+        print(Fore.BLUE + f"Current scores: {player_score}:{computer_score}" + Style.RESET_ALL)
+    else:
+        computer_score += 1
+        print(Fore.RED + "You lose!" + Style.RESET_ALL)
+        print(Fore.LIGHTYELLOW_EX + f"Current scores: {player_score}:{computer_score}" + Style.RESET_ALL)
+
+    request = input("Would you like to continue playing? (Yes or No): ")
+
+if request == "No":
+    print(Fore.BLUE + "Thank you for playing Rock-Paper-Scissors!" + Style.RESET_ALL)
+    print(Fore.GREEN + "Have a nice day!" + Style.RESET_ALL)
 else:
-    raise SystemExit("Invalid Input. Please try again...")
-
-computer_random_number = randint(1, 3)
-computer_move = ""
-
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
-
-print(f"The computer chose {computer_move}.")
-
-if (player_move == rock and computer_move == scissors) \
-        or (player_move == paper and computer_move == rock) \
-        or (player_move == scissors and computer_move == paper):
-    print("You win!")
-elif player_move == computer_move:
-    print("Draw!")
-else:
-    print("You lose!")
-    
+    raise SystemExit("Invalid Input")
